d45df2aa705029a8583bc103f3709f444ea41f29
Rodrigo <rodrigo.estroz+test@gmail.com>
2026-03-08 23:54:18 +0000

    primeros ejercicios, muestra por pantalla saludo, muestra var nombre ciudad y edad, suma 2 valores

---
+++ primeros_pasos.py
@@ -0,0 +1,19 @@
+
+
+print ("¡Hola! Este es mi primer programa en Python.")
+
+nombre = "Rodrigo Estroz Guerrero" #variable tipo str, texto
+edad = 47                          #variable tipo entero
+ciudad = "Valdivia"                #variable tipo entero
+
+print(nombre, edad, ciudad)        #mostrara por pantalla el valor de nombre,edad y ciudad
+
+a = 10 #variable tipo entero
+b = 7  #variable tipo entero
+
+print(a+b) #mostrara por pantalla la suma de a + b
+
+
+
+
+
