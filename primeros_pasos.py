3e0295866cb8640afbda52e1c37042e8cc51169c
Rodrigo <rodrigo.estroz+test@gmail.com>
2026-03-09 00:27:25 +0000

    se calcula raiz cuadrada de 25 con funcion math.sqrt

--- primeros_pasos.py
+++ primeros_pasos.py
@@ -1,17 +1,28 @@
 
 
-print ("¡Hola! Este es mi primer programa en Python.")
+print ("¡Hola! Este es mi primer programa en Python.") # mostrara por pantalla el saludo
 
-nombre = "Rodrigo Estroz Guerrero" #variable tipo str, texto
-edad = 47                          #variable tipo entero
-ciudad = "Valdivia"                #variable tipo entero
+nombre = "Rodrigo Estroz Guerrero" # variable tipo str, texto
+edad = 47                          # variable tipo entero
+ciudad = "Valdivia"                # variable tipo entero
 
-print(nombre, edad, ciudad)        #mostrara por pantalla el valor de nombre,edad y ciudad
+print(nombre)   # mostrara por pantalla el valor de nombre
+print(edad)     # mostrara por pantalle el valor de edad
+print(ciudad)   # mostrara por pantalla el valor de ciudad
 
-a = 10 #variable tipo entero
-b = 7  #variable tipo entero
 
-print(a+b) #mostrara por pantalla la suma de a + b
+a = 10          # variable tipo entero
+b = 7           # variable tipo entero
+
+print (a+b) # mostrara por pantalla la suma de a + b
+
+import math  # se importa funcion math para calculos matematicos
+
+resultado = (math.sqrt(25))
+
+print(resultado)
+
+
 
 
 
